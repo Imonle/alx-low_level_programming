511ec1df2cf20e259597baaff2210e6e51cbe465
Joshua Osemenho <osemenhojoshua@gmail.com>
2022-09-18 16:13:51 -0700

    Write a function that checks for uppercase character.
    Prototype: int _isupper(int c);
    Returns 1 if c is uppercase
    Returns 0 otherwise
    FYI: The standard library provides a similar function: isupper. Run man isupper to learn more.

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,10 +5,15 @@
  * @c: input
  * Return: 1 if c is uppercase, 0 otherwise
  */
+
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
-		return (1);
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
 	else
-		return (0);
+	{
+	return (0);
+	}
 }
